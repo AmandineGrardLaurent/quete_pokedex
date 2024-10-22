7d67024ec1c6134798997593e7ae1e168f59a816
Amandine GRARD-LAURENT <a.grard.laurent08@gmail.com>
2024-10-22 16:19:11 +0200

    feat: fin de quête 3

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,8 +1,24 @@
+const pokemonList = [
+	{
+		name: "bulbasaur",
+		imgSrc:
+			"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+	},
+	{
+		name: "mew",
+	},
+];
+
 function PokemonCard() {
+	const pokemon = pokemonList[0];
 	return (
 		<figure>
-			<img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"></img>
-			<figcaption>Bulbizarre</figcaption>
+			{pokemon.imgSrc === undefined ? (
+				<p>"???"</p>
+			) : (
+				<img src={pokemon.imgSrc}></img>
+			)}
+			<figcaption>{pokemon.name}</figcaption>
 		</figure>
 	);
 }
