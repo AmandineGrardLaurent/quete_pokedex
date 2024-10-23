a8515465dc715d7580c65796a1f057abe9756674
Amandine GRARD-LAURENT <a.grard.laurent08@gmail.com>
2024-10-23 16:28:23 +0200

    feat: fin de quête 5

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,10 +1,15 @@
-function PokemonCard({ pokemon }) {
+type pokemonProps = {
+	name: string;
+	imgSrc?: string;
+};
+
+function PokemonCard({ pokemon }: { pokemon: pokemonProps }) {
 	return (
 		<figure>
 			{pokemon.imgSrc === undefined ? (
 				<p>"???"</p>
 			) : (
-				<img src={pokemon.imgSrc}></img>
+				<img src={pokemon.imgSrc} alt={pokemon.name} />
 			)}
 			<figcaption>{pokemon.name}</figcaption>
 		</figure>
